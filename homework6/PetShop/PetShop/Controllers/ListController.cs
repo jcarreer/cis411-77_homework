82b84e80fa0a161615cfc002832c7e0694437e12
jcarreer <josh.carreer@gmail.com>
2014-03-18 14:32:39 -0400

    The PetShop Homework

---
+++ homework6/PetShop/PetShop/Controllers/ListController.cs
@@ -0,0 +1,38 @@
+﻿using PetShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetShop.Controllers
+{
+    public class ListController : Controller
+    {
+        private PetDBContext db = new PetDBContext();
+
+      
+        public ActionResult Index()
+        {
+            return View(db.Pets.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Pet pet = db.Pets.Find(id);
+
+            if (pet == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(pet);
+        }
+    }
+}
---
+++ homework6/PetShop/PetShop/Models/Pet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PetShop.Models
+{
+    public class Pet
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Image URL")]
+        [DataType(DataType.ImageUrl)]
+        public String imageUrl { get; set; }
+        [Display(Name = "Name")]
+        public String name { get; set; }
+
+        [Display(Name = "Date Recieved")]
+        [DataType(DataType.Date)]
+        public DateTime dateRecieved { get; set; }
+
+        [Display(Name = "Short Description")]
+        public String descriptionShort { get; set; }
+
+        [Display(Name = "Full Description")]
+        public String descriptionFull { get; set; }
+
+        [Display(Name = "Price")]
+        [DataType(DataType.Currency)]
+        public decimal price { get; set; }
+
+        [Display(Name = "Quantity")]
+        public int quantity { get; set; }
+    }
+
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
